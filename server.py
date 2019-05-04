1b3d8e35d5fc8b5d117305f41b1d26b7faf1e899
julesboulay <jules.boulaydetouchet@mail.mcgill.ca>
2019-05-04 14:22:26 +0800

    Fixed json bug

--- server.py
+++ server.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def hello_world():
     if request.method == 'POST':
-        data = request.get_json(silent=True)
-        buffer = data.get("data")
-        print(buffer)
-        print(len(buffer))
+        buffer = request.json.get("data")
+        print("Lenght of Buffer: " + str(len(buffer)))
         return jsonify(marzocco_probability=0.8)
 
 if __name__ == '__main__':
