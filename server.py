7e46277c632d76aacb57f6fc7c090dfefa37ade2
julesboulay <jules.boulaydetouchet@mail.mcgill.ca>
2019-05-04 14:39:20 +0800

    Fixed bug

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, Response
+import json
 
 app = Flask(__name__)
 
@@ -8,8 +9,8 @@ def hello_world():
         buffer = request.json.get("data")
         print("Lenght of Buffer: " + str(len(buffer)))
         
-        json_res = jsonify(marzocco_probability=0.8)
-        return Response(json_res, mimetype='application/json')
+        json_res = {"marzocco_probability": 0.8}
+        return Response(json.dumps(json_res), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
