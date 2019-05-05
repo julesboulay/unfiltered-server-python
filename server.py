465ab4976fb8eccb2398786690bce24bd32a20ff
julesboulay <jules.boulaydetouchet@mail.mcgill.ca>
2019-05-05 20:44:15 +0800

    Added mock python server request

--- server.py
+++ server.py
@@ -6,11 +6,11 @@ from tensorflow import keras
 import numpy as np
 import zipfile
 import sys
-
-#sys.path.append('/usr/local/lib/python3.7/site-packages')
+import random
+sys.path.append('/usr/local/lib/python3.7/site-packages')
 import cv2
 
-PHOTODIR = os.path.join(os.getcwd(), "server_image/photo.jpg")
+PHOTODIR = os.path.join(os.getcwd(), "server_image")
 MODELDIR = os.path.join(os.getcwd(), "model_marzocco_detector.h5")
 IMG_SIZE = 100
 
@@ -26,36 +26,60 @@ test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def hello_world():
+def poredict_image():
     if request.method == 'POST':
         # 1. Get JSON base64 data
-        content = request.get_json(silent=True)
-        buffer = content['data']
+        buffer = ""
+        photoid = ""
+        if request.data:
+                content = request.get_json(silent=True)
+                photoid = content['photo_id']
+                buffer = content['data']
+        else:
+                json_res = {"message": "failure", "error": "No data sent"}
+                return Response(json.dumps(json_res), mimetype='application/json')
         
+
         # 2. Decode Image and Save
-        image_64_decode = base64.b64decode(buffer)
-        image_result = open(PHOTODIR, "wb") 
-        image_result.write(image_64_decode)
+        photo_path = os.path.join(PHOTODIR, photoid + ".jpg")
+        try:
+                image_64_decode = base64.b64decode(buffer)
+                image_result = open(photo_path, "wb")
+                image_result.write(image_64_decode)
+        except Exception as e:
+                json_res = {"message": "failure", "error": "Buffer sent not image compatible"}
+                return Response(json.dumps(json_res), mimetype='application/json')
 
         # 3. Load Image Array
-        img_array = cv2.imread(PHOTODIR, cv2.IMREAD_GRAYSCALE)
-        new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-
         x = []
         try:
-                img_array = cv2.imread(PHOTODIR, cv2.IMREAD_GRAYSCALE)
+                img_array = cv2.imread(photo_path, cv2.IMREAD_GRAYSCALE)
                 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
                 x.append([new_array])
         except Exception as e:
-                pass
-        
-        # 4. Magic
-        x = np.array(x).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        pred = model.predict(test_datagen.flow(x, batch_size=1)[0])[0][0].item()
+                json_res = {"message": "failure", "error": "Error loading image into array"}
+                return Response(json.dumps(json_res), mimetype='application/json')
 
-        json_res = {"marzocco_probability": pred}
+        # 4. Magic
+        pred = 0
+        try:
+                x = np.array(x).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+                pred = model.predict(test_datagen.flow(x, batch_size=1)[0])[0][0].item()
+        except Exception as e:
+                json_res = {"message": "failure", "error": "Error calculating prediction"}
+                return Response(json.dumps(json_res), mimetype='application/json')
+
+        json_res = {"message": "success", "marzocco_probability": pred}
         return Response(json.dumps(json_res), mimetype='application/json')
+
+
+@app.route('/mock', methods=['POST'])
+def poredict_image_mock():
+    if request.method == 'POST':
+        mock_pred = random.random()
+        json_res = {"message": "success", "marzocco_probability": mock_pred}
+        return Response(json.dumps(json_res), mimetype='application/json')
+
 
 if __name__ == '__main__':
     app.run()
-
